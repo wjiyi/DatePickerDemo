a1f32d97de8a3ac38a0f857e44e051c92412c7c1
wjy <1096494284@qq.com>
2018-04-23 22:00:49 +0800

    第一次提交

---
+++ app/src/main/java/com/example/administrator/datepickerdemo/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.administrator.datepickerdemo;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.icu.util.Calendar;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatePicker datePicker;
+    private TimePicker timePicker;
+    private Calendar cal;
+    private int year;
+    private int month;
+    private int day;
+    private int hour;
+    private int minute;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cal = Calendar.getInstance();
+        year = cal.get(Calendar.YEAR);
+        month = cal.get(Calendar.MONTH)+1;
+        day = cal.get(Calendar.DAY_OF_MONTH);
+        hour = cal.get(Calendar.HOUR_OF_DAY);
+        minute = cal.get(Calendar.MINUTE);
+        setTitle(year+"-"+month+"-"+day+"-"+hour+":"+minute);
+        datePicker = (DatePicker) findViewById(R.id.datePicker);
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+        datePicker.init(year, cal.get(Calendar.MONTH), day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                setTitle(year+"-"+(monthOfYear+1)+"-"+dayOfMonth);
+            }
+        });
+
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                setTitle(hourOfDay+":"+minute);
+            }
+        });
+
+//        new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+//            @Override
+//            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+//                setTitle(year+"-"+(month+1)+"-"+dayOfMonth);
+//            }
+//        }, year, cal.get(Calendar.MONTH), day).show();
+
+        new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                setTitle(hourOfDay+":"+minute);
+            }
+        }, hour, minute, true).show();
+
+    }
+}
